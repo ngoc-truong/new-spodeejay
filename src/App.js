46e1907be45d839bbd026c56abc22f36b23918db
Ngoc Truong <ngoc.truong@posteo.de>
2022-02-17 21:47:51 +0100

    Load playlists on login

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const AUTH_ENDPOINT = process.env.REACT_APP_AUTH_ENDPOINT;
 const CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
@@ -6,13 +8,75 @@ const REDIRECT_URI = process.env.REACT_APP_REDIRECT_URI;
 const RESPONSE_TYPE = process.env.REACT_APP_RESPONSE_TYPE;
 
 function App() {
+  const [token, setToken] = useState("");
+  const [playlists, setPlaylist] = useState("");
+
+  // Get the returned token from spotify
+  useEffect(() => {
+    // We take the access token from the URL of the window (in the hash)
+    const hash = window.location.hash;
+    let token = window.localStorage.getItem("token");
+
+    if (!token && hash) {
+      token = hash
+        .substring(1)
+        .split("&")
+        .find((element) => element.startsWith("access_token"))
+        .split("=")[1];
+
+      window.location.hash = "";
+      window.localStorage.setItem("token", token);
+    }
+
+    setToken(token);
+  }, []);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const { data } = await axios.get(
+        "https://api.spotify.com/v1/me/playlists",
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      setPlaylist(data);
+    };
+    console.log(`Was geht hier gerade ab: ${token}`);
+    fetchData();
+  }, [token]);
+
+  const renderPlaylists = () => {
+    if (!playlists) {
+      return <p>Sorry, keine Playlists vorhanden.</p>;
+    } else {
+      return playlists.items.map((playlist) => (
+        <p key={playlist.id}>{playlist.name}</p>
+      ));
+    }
+  };
+
+  const logout = () => {
+    setToken("");
+    window.localStorage.removeItem("token");
+  };
+
   return (
     <div>
-      <a
-        href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}
-      >
-        Login to Spotify
-      </a>
+      <h1>Spodeejay</h1>
+      {!token ? (
+        <a
+          href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}
+        >
+          Login
+        </a>
+      ) : (
+        <div>
+          <button onClick={logout}>Logout</button>
+          {renderPlaylists()}
+        </div>
+      )}
     </div>
   );
 }
